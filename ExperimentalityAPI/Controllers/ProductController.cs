a5409d7a161217c4b0fa51e82890f64e593d9be4
jaimeaRios <jaimearios1986@gmail.com>
2021-07-12 18:10:44 -0500

    delete start class and controller. Add Model for Products and its controller

---
+++ ExperimentalityAPI/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models.Model;
+
+namespace ExperimentalityAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Segunda prueba
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("load")]
+        public IActionResult Upload(IFormFile file)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("createProduct")]
+        public IActionResult createProduct([FromForm] Product product)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/Model/Product.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Model
+{
+    /// <summary>
+    /// Products 
+    /// </summary>
+    public class Product
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public double price { get; set; }
+        public double discountPercentage { get; set; }
+        public IFormFile frontImage { get; set; }
+        public IFormFile backImage { get; set; }
+        public string country { get; set; }
+    }
+}
